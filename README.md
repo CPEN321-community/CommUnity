7681cf49508e52d0eda6432813ce7463bae77f88
JoshuaParkSJ <jpark9061@gmail.com>
2022-06-25 23:20:37 -0700

    set up backend with cors, nodemon, and router

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 # Technology Stack
 
-Android frontend in Java, backend in Node.js, and database in MySQL.
+Community consiste of an Android frontend (Java), Node.js backend, and MySql database. Community will be hosted through AWS.
 
 
 
--- backend/controllers/exampleController.js
+++
@@ -1,9 +0,0 @@
-const cartServices = require('../services/cartService');
-
-const addCartController = async (req, res) => {
-  const { id } = req.body;
-  const response = await cartServices.addToCart(id);
-  res.json(response);
-};
-
-module.exports = addCartController;
