3f465ada6414d666b72a71c7dd341de2198ebb37
cxsong1 <csca12.cs@gmail.com>
2022-07-05 16:51:58 -0400

    Basic layout complete

---
+++ frontend/requests_page/app/src/main/java/com/example/request_posts_page/request_page.java
@@ -0,0 +1,14 @@
+package com.example.request_posts_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class request_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.request_page);
+    }
+}
