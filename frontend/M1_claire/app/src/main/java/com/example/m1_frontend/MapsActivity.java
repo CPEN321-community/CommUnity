3e1212cc35e33c9407349879a71601172578eeba
cxsong1 <csca12.cs@gmail.com>
2022-07-04 21:05:11 -0400

    M1 frontend code

---
+++ frontend/M1_claire/app/src/main/java/com/example/m1_frontend/MapsActivity.java
@@ -0,0 +1,76 @@
+package com.example.m1_frontend;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.m1_frontend.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+    LocationManager locationManager;
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    final static String TAG = "MapActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        //Error below can be ignored following Piazza post @29
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Toronto (favourite city!) and move the camera
+        LatLng toronto = new LatLng(43.6532, -79.3832);
+        mMap.addMarker(new MarkerOptions().position(toronto).title("Toronto, ON"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(toronto));
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        //Log current location
+        Log.d(TAG, "Lat: " + location.getLatitude() + " | Long: " + location.getLongitude());
+    }
+
+}
