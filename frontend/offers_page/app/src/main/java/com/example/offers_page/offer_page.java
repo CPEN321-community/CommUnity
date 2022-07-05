0fcead9f25c9a238cc9bbb3f9035b4c3ed1e5595
cxsong1 <csca12.cs@gmail.com>
2022-07-05 17:10:34 -0400

    Offer page basic layout

---
+++ frontend/offers_page/app/src/main/java/com/example/offers_page/offer_page.java
@@ -0,0 +1,14 @@
+package com.example.offers_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class offer_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.offer_page);
+    }
+}
