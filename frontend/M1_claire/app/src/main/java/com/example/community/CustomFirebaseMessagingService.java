2aaaa44846413bfd2e25fa0190475a020a20d832
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2022-07-08 18:37:34 -0700

    re-add notification to frontend

---
+++ frontend/M1_claire/app/src/main/java/com/example/community/CustomFirebaseMessagingService.java
@@ -0,0 +1,32 @@
+package com.example.community;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class CustomFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FIREBASE_MESSAGING_SERVICE";
+
+    /**
+     * There are two scenarios when onNewToken is called:
+     * 1) When a new token is generated on initial app startup
+     * 2) Whenever an existing token is changed
+     * Under #2, there are three scenarios when the existing token is changed:
+     * A) App is restored to a new device
+     * B) User uninstalls/reinstalls the app
+     * C) User clears app data
+     */
+    @Override
+    public void onNewToken(@NonNull String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        super.onNewToken(token);
+//        TODO send to database
+    }
+}
