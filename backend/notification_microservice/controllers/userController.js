0a072a443dca5c8d39e595177a72962c3d2c1b50
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2022-07-08 17:21:53 -0700

    added notification service

---
+++ backend/notification_microservice/controllers/userController.js
@@ -0,0 +1,61 @@
+const {User} = require("../models")
+const admin = require('firebase-admin/app');
+const {getMessaging} = require("firebase-admin/messaging")
+var serviceAccount = require("../firebase_service_key.json");
+
+admin.initializeApp({
+  credential: admin.cert(serviceAccount)
+});
+const FCM = getMessaging();
+
+const createUser = async (req, res) => {
+  try {
+
+    const {userId, token} = req.body;
+    const [data, created] = await User.upsert({
+      userId,
+      token,
+    });
+    if (created) {
+      res.sendStatus(201);
+      return;
+    }
+    else {
+      res.sendStatus(200);
+    }
+  }
+  catch (e) {
+    console.error(e);
+    res.status(500).json({
+      error: e
+    })
+  }
+}
+  
+const sendNotifToUser = async (req, res) => {
+  const {userId, payload} = req.body;
+  try {
+    const user = await User.findOne({where: {userId}});
+    console.log(user);
+    const token = user.dataValues.token;
+    await sendNotif(token, payload);
+
+    res.sendStatus(201);
+  }
+  catch (e) {
+    console.error(e);
+    res.status(500).json({
+      error: e
+    });
+  }
+
+}
+
+const sendNotif = async (token, payload) => {
+  const notif = {
+    notification: payload
+  }
+  return await FCM.sendToDevice(token, notif)
+}
+
+module.exports = {createUser, sendNotifToUser}
---
+++ backend/notification_microservice/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const cors = require('cors');
+const db = require('./models');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+app.use(routes);
+
+db.sequelize.sync().then((req) => {
+  // app.listen(3031, () => {
+  //   console.log("MySQL server running on http://localhost:3031");
+  // })
+}).catch(e => console.log(e));
+
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
