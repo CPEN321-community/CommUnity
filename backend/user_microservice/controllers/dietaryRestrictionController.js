47cf06df4de735cafdc523e8c2843d2f13fee38d
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2022-07-10 13:04:17 -0700

    finish stats and dietary restrictions

---
+++ backend/user_microservice/controllers/dietaryRestrictionController.js
@@ -0,0 +1,62 @@
+const sequelize = require("sequelize");
+const { DietaryRestriction } = require("../models");
+
+const getUserDietaryRestrictions = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    console.log("userId", userId);
+
+    let restrictions = await DietaryRestriction.findAll({
+      where: {
+        userId,
+      },
+    });
+    console.log(restrictions);
+
+    res.json(restrictions);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+};
+
+const createDietaryRestriction = async (req, res) => {
+  try {
+    const {userId, name} = req.body;
+    console.log("userId", userId);
+    let createdRestriction = await DietaryRestriction.create({
+      UserUserId: userId,
+      name
+    });
+    console.log(createdRestriction);
+
+    res.json(createdRestriction.dataValues);
+  }
+  catch(e) {
+    res.sendStatus(500);
+  }
+}
+
+
+const deleteDietaryRestriction = async (req, res) => {
+  try {
+    const {id} = req.body;
+    console.log(id);
+    let deleted = await DietaryRestriction.destroy({
+      where: {id}
+    });
+
+    res.json({deleted});
+  }
+  catch(e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+}
+
+
+module.exports = {
+  deleteDietaryRestriction,
+  createDietaryRestriction,
+  getUserDietaryRestrictions
+}
