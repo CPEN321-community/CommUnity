30cb5aa90db73073e1003deca7ebcd9b15f5757a
JoshuaParkSJ <jpark9061@gmail.com>
2022-07-13 21:23:23 -0700

    add github workflow

--- backend/recommendation_microservice/test/recommendationController.spec.js
+++ backend/recommendation_microservice/test/recommendationController.spec.js
@@ -4,11 +4,15 @@ describe("Recommendation Controller", () => {
   it("triggers the mock for getting suggested request posts", () => {
     let result = getSuggestedRequests('peepeepoopoo');
     expect(result).toEqual([{postId: 'post1', score: 100}, { postId: 'post2', score: 50}]);
+    expect(getSuggestedRequests).toHaveBeenCalledWith('peepeepoopoo');
+    expect(getSuggestedRequests).toHaveBeenCalledTimes(1);
   });
 
   it("triggers the mock for getting suggested offer posts", () => {
     let result = getSuggestedOffers('peepeepoopoo');
     expect(result).toEqual([{postId: 'post1', score: 100}, { postId: 'post2', score: 50}]);
+    expect(getSuggestedOffers).toHaveBeenCalledWith('peepeepoopoo');
+    expect(getSuggestedOffers).toHaveBeenCalledTimes(1);
   });
 
   it("triggers the mock for deleting request posts", () => {
@@ -21,10 +25,3 @@ describe("Recommendation Controller", () => {
     expect(result).toBe(true);
   });
 });
-
-// router.get('/suggestedPosts/request/:item', getSuggestedPosts);
-// router.get('/suggestedPosts/offer/:item', getSuggestedPosts);
-
-// router.delete('/suggestedPosts/request/:postId', deletePost);
-// router.delete('/suggestedPosts/offer/:postId', deletePost);
-
