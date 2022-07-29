4b177facdf567bf98db008c79f4f58d25b7f28bf
JoshuaParkSJ <jpark9061@gmail.com>
2022-07-28 19:59:12 -0700

    unify config structure

--- backend/user_microservice/models/index.js
+++ backend/user_microservice/models/index.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
-const config = require("./../config/config.json")[env];
+const config = require("./../../config_user.json")[env];
 const db = {};
 
 function applyRelationships(sequelize) {
