6346baaafb95614e5a54be6749fe15a2c1cbd6d5
JoshuaParkSJ <jpark9061@gmail.com>
2022-08-07 12:38:39 -0700

    fix eslint again

--- backend/post_microservice/controllers/offerPostController.js
+++ backend/post_microservice/controllers/offerPostController.js
@@ -131,7 +131,7 @@ const searchOffersWithTags = async (req, res) => {
 
 const createOffer = async (req, res) => {
     const hasAllFields = req.body.userId && req.body.title && req.body.description && req.body.quantity && req.body.pickUpLocation && req.body.status && req.body.bestBeforeDate && req.body.tagList;
-    const validDate = req.body.bestBeforeDate.length == 10;
+    const validDate = req.body.bestBeforeDate.length === 10;
 
     if(hasAllFields && validDate) {
         const createdOffer = await OfferPost.create({
